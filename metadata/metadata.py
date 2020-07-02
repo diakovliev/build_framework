7d6bc840970fbde5b1b857a05d826fac1eef4996
dmytro.iakovliev <dmytro.iakovliev@dev.zodiac.tv>
2020-07-02 14:01:49 +0300

    Remove raad details from metadata framework

--- metadata/metadata.py
+++ metadata/metadata.py
@@ -12,17 +12,6 @@ class ReadFile:
     def filename(self):
         return self.__filename
 
-    def read(self):
-        ret = ""
-
-        if not os.path.isfile(self.__filename):
-            raise Exception("No such file: %s!" % self.__filename)
-
-        with open(self.__filename, 'r') as f:
-            ret = f.read()
-
-        return ret
-
 
 class GetEnv:
     """
@@ -34,11 +23,3 @@ class GetEnv:
 
     def env_name(self):
         return self.__env_name
-
-    def read(self):
-        ret = os.environ.get(self.__env_name, None)
-
-        if ret is None:
-            raise Exception("Can't find: %s environment variable!" % self.__env_name)
-
-        return ret
